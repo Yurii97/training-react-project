f0352bfad84471fe9399fe9182ef378b6be59374
Yurii Onats <yura.onats@gmail.com>
2021-12-05 22:58:13 +0200

    day1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import paintings from './paiting.json'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const data = paintings[0];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Painting(props){
+  return <div>
+  <img src = {props.url} alt={props.title} width="480"/>
+  <h2>{data.title}</h2>
+  <p>
+    Author: <a href={data.author.url}>{props.author.tag}</a>
+  </p>
+  <p>
+    Price: {props.price}, UAH
+  </p>
+  <p>
+  Availability: {props.quantity} 
+  </p>
+  <button>Add to cart</button>
+</div>
+}
+
+ReactDOM.render(<Painting />, document.querySelector('#root'))
+
+//=========================================
+
+// const penting = (
+// <div>
+//   <img src = {data.url} alt={data.title} width="480"/>
+//   <h2>{data.title}</h2>
+//   <p>
+//     Author: <a href={data.author.url}>{data.author.tag}</a>
+//   </p>
+//   <p>
+//     Price: {data.price}, UAH
+//   </p>
+//   <p>
+//   Availability: {data.quantity} 
+//   </p>
+//   <button>Add to cart</button>
+// </div>
+// );
+
+// ReactDOM.render(penting, document.querySelector('#root'))
+
+//====================================
+
+// const element1 = <span>Hello</span>;
+// const element2 = <span>World</span>;
+
+// // const element = React.createElement('div', {a:5, b:10, children: 'Hello World'});
+
+// const element = (
+//   <div>
+//     {element1}
+//     {element2}
+//   </div>
+// );
+
+// console.log(element);
+
+// ReactDOM.render(element, document.querySelector('#root'))
+
+//==================================
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+//===========================
