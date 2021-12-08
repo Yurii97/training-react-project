d3da94aa8d1e1ef88f1046e94ef439d6ca0aefdd
Yurii Onats <yura.onats@gmail.com>
2021-12-08 22:57:16 +0200

    add

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,15 +1,18 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
-function Section ({title, children}){
-    return (
-        <div>
-        {title && <h2>{title}</h2>}{children}
-        </div>
-    )
+function Section({ title, children }) {
+  return (
+    <div>
+      {title && <h2>{title}</h2>}
+      {children}
+    </div>
+  );
 }
 
-Section.propTypes={
-    title: PropTypes.string,
-}
+Section.propTypes = {
+  title: PropTypes.string,
+};
 
+const rss = false;
+console.log(rss);
 export default Section;
